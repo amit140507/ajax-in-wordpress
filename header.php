d818547b3a4d3778d7804184625ca351d9ec1bcc
amit140507 <100019842+amit140507@users.noreply.github.com>
2022-09-30 11:40:47 +0530

    Update header.php

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 //PUT THIS CODE INSIDE <head> </head> TAG.
 
-		<script type="text/javascript">
-    var ajaxurl = "<?php echo admin_url('admin-ajax.php'); ?>";
+<script type="text/javascript">
+var ajaxurl = "<?php echo admin_url('admin-ajax.php'); ?>";
 </script>
