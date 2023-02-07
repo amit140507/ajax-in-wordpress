e1f6b9f6751a138d9a09f79951846bf059d40b8f
amit140507 <100019842+amit140507@users.noreply.github.com>
2023-02-07 16:00:32 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,34 +1,41 @@
-	function new_enqueue_style_scripts()
-{
-$style_ver = filemtime( get_stylesheet_directory() . '/assets/js/custom.js' );
-wp_register_script('new_custom_js', get_stylesheet_directory_uri().'/assets/js/custom.js', array(), $style_ver);
-wp_enqueue_script('new_custom_js');
-}
-add_action('wp_enqueue_scripts', 'new_enqueue_style_scripts');
+function custom_enqueue_script() {
+	wp_enqueue_script('jquery');   // Use this if wanna use build in Jquery Version.
+	wp_register_script('jquery_validate', 'https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/jquery.validate.min.js', array(), '1.19.5', false );
+	wp_register_script( 'custom-script', get_template_directory_uri() . '/assets/js/custom.js', array(), '1.0.0', true ); 
+	wp_enqueue_script('jquery_validate');
+	wp_enqueue_script( 'custom-script');
 
+	wp_localize_script( 'custom-script', 'customform', array(    'ajaxurl' => admin_url( 'admin-ajax.php' )));
+	
+}
+add_action( 'wp_enqueue_scripts', 'custom_enqueue_script' );
+
+
+// add_action('wp_head', 'custom_ajaxurl');
+// function custom_ajaxurl() {
+//    echo '<script type="text/javascript">
+//            var ajaxurl = "' . admin_url('admin-ajax.php') . '";
+//          </script>';
+// }
 
 
 // AJAX CODE 
 
-wp_localize_script( 'new_custom_js', 'ajax', array(
-    'url' => admin_url( 'admin-ajax.php' )
-) );
-
-add_action('wp_ajax_payment_processing_data', 'payment_processing_data');
-add_action('wp_ajax_nopriv_payment_processing_data', 'payment_processing_data');
-
-function payment_processing_data()
+add_action('wp_ajax_form_submit_ajax', 'form_submit_ajax_function');
+add_action('wp_ajax_nopriv_form_submit_ajax', 'form_submit_ajax_function');
+function form_submit_ajax_function()
 {
-	
- $first_name_user = $_POST['first_name_user'];
- $last_name_user = $_POST['last_name_user'];
- $email_forpay = $_POST['email_forpay'];
- $ammount_forpay = $_POST['ammount_forpay'];
- $pin_code_forpay = $_POST['pin_code_forpay'];
- $phone_num_forpay = $_POST['phone_num_forpay'];
+   
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$email = $_POST['email'];
+$amount_forpay = $_POST['amount'];
+$zipcode = $_POST['zipcode'];
+$phone_number = $_POST['phone_number'];
 
+print_r($first_name);
 // CUSTOM CODE START HERE
+//CUSTOM CODE END HERE	
 
-//CUSTOM CODE END HERE
-
+wp_die();
 }
