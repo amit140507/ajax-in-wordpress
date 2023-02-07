02d548170b606044cf8bf282f2f1c9859196aff6
amit140507 <100019842+amit140507@users.noreply.github.com>
2023-02-07 15:59:51 +0530

    Update form.php

--- form.php
+++ form.php
@@ -1,51 +1,41 @@
 <?php 
-    /* Template Name: payment-template */
+    /* Template Name: Form Template */
     get_header();
 ?>
 <div class="container">
-    <div class="payment-page-cooling-text">
-    <p>
-   Custom Form</p>
-</div>
-    <div class="payment-inner-section">
-    <form method="post" action="" class="field-all-form">
-         <div class="email-field">
-            <label for="first_name_user">First Name</label>
-            <input type="text" name="first_name_user" class="field-name-pay"/>
-        </div>
-         <div class="email-field">
-            <label for="last_name_user">Last Name</label>
-            <input type="text" name="last_name_user" class="field-last-pay"/>
-        </div>
-        <div class="email-field">
-            <label for="email_forpay">Email</label>
-            <input type="email" name="email_forpay" class="field-email-pay"/>
-        </div>
-        
-        <div class="ammount-field">
-            <label for="ammount_forpay">Amount (In USD)</label>
-            <input type="text" name="ammount_forpay" class="field-ammount-pay"/>
-        </div>
-         <div class="email-field">
-            <label for="pin_code_forpay">Zip Code</label>
-            <input type="text" name="pin_code_forpay" class="field-pin-pay"/>
-        </div>
-        <div class="email-field">
-            <label for="phone_num_forpay">Phone number</label>
-            <input type="text" name="phone_num_forpay" class="field-phone-pay"/>
-        </div>
-
-        
-        <input type="submit" value="Submit" name="btn_forpay" class="btn-submit-pay"/>
-            
-        <div class="data-output">
-            <?php echo $output; ?>
-        </div>
-        
-    </form>
-    
-</div>
+    <div class="title-div">
+        <p>Custom Form</p>
     </div>
+    <div class="inner-section">
+        <form method="post" action="" class="form-class" id="form-id">
+            <div class="first-name-field">
+                <label for="first_name">First Name</label>
+                <input type="text" name="first_name" class="first-name-class"/>
+            </div>
+            <div class="last-name-field">
+                <label for="last_name">Last Name</label>
+                <input type="text" name="last_name" class="last-name-class"/>
+            </div>
+            <div class="email-field">
+                <label for="email">Email</label>
+                <input type="email" name="email" class="email-class"/>
+            </div>
+            <div class="amount-field">
+                <label for="amount">Amount (In USD)</label>
+                <input type="text" name="amount" class="amount-class"/>
+            </div>
+            <div class="zipcode-field">
+                <label for="zipcode">Zip Code</label>
+                <input type="text" name="zipcode" class="zipcode-class"/>
+            </div>
+            <div class="phone-number-field">
+                <label for="phone_number">Phone number</label>
+                <input type="text" name="phone_number" class="phone-number-class"/>
+            </div>
+            <input type="submit" value="Submit" name="button_submit" class="submit-class"/>
+        </form>
+    </div>
+</div>
     
  
 <?php
